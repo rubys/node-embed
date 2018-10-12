41e156ce06b535f33a96467e7afce434cbb321cd
Sam Ruby <rubys@intertwingly.net>
2018-10-12 09:30:40 -0700

    add test and clean targets

--- mkmf.js
+++ mkmf.js
@@ -21,10 +21,24 @@ out += input.match(/CFLAGS_CC_Release.*?\n\n/s)[0];
 input = fs.readFileSync(srcdir + "/out/node.target.mk", "utf8");
 out += input.match(/LDFLAGS_Release.*?\n\n/s)[0];
 out += input.match(/LIBS.*?\n\n/s)[0];
-out += input.match(/LD_INPUTS.*?\n/)[0] + "\n";
+out += input.match(/LD_INPUTS.*?\n/)[0];
 
-out += "node_main: node_main.o node.o\n" + "\tc++ $(LDFLAGS_Release) -o $@ $+ $(LIBS) $(LD_INPUTS)\n\n";
-out += "node.o: node.cc node_embed.h\n" + "\tc++ $(DEFS_Release) $(INCS_Release) $(CFLAGS_Release) \\\n" + "\t$(CFLAGS_CC_Release) -c $< -o $@\n\n";
-out += "node_main.o: node_main.c node_embed.h\n" + "\tcc $(CFLAGS_Release) -c $< -o $@\n";
+out += `
+node_main: node_main.o node.o
+\tc++ $(LDFLAGS_Release) -o $@ $+ $(LIBS) $(LD_INPUTS)
+
+node.o: node.cc node_embed.h
+\tc++ $(DEFS_Release) $(INCS_Release) $(CFLAGS_Release) \\
+\t$(CFLAGS_CC_Release) -c $< -o $@
+
+node_main.o: node_main.c node_embed.h
+\tcc $(CFLAGS_Release) -c $< -o $@
+
+test:
+\ttest \`./node_main\` = "2" && echo success
+
+clean:
+\trm -f *.o nodemain
+`;
 
 fs.writeFileSync("Makefile", out)
