3d3b5f2ce959be23b6acb673a799847bb242f9ba
Sam Ruby <rubys@intertwingly.net>
2018-10-13 10:02:35 -0700

    add a conspicuous DO NOT USE warning

--- node_embed.h
+++ node_embed.h
@@ -1,4 +1,24 @@
-// node_embed.h
+// node_embed.h : an API for Node embedders
+
+/*
+
+ ______   _______    _        _______ _________            _______  _______
+(  __  \ (  ___  )  ( (    /|(  ___  )\__   __/  |\     /|(  ____ \(  ____ \
+| (  \  )| (   ) |  |  \  ( || (   ) |   ) (     | )   ( || (    \/| (    \/
+| |   ) || |   | |  |   \ | || |   | |   | |     | |   | || (_____ | (__
+| |   | || |   | |  | (\ \) || |   | |   | |     | |   | |(_____  )|  __)
+| |   ) || |   | |  | | \   || |   | |   | |     | |   | |      ) || (
+| (__/  )| (___) |  | )  \  || (___) |   | |     | (___) |/\____) || (____/\
+(______/ (_______)  |/    )_)(_______)   )_(     (_______)\_______)(_______/
+
+This header is beyond experimental status at this point.  Change is not only
+likely, it is all but certain.  At a minimum, it is planned to become
+N-API'ized, but more significantly the immediate plans are to focus on teasing
+apart the per-process setup and teardown away from the per isolate setup and
+teardown, and that likely will require substantial changes.
+
+*/
+
 #ifdef __cplusplus
 extern "C" {
 #endif
